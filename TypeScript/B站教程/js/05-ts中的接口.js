03ccc46db8fef64824e615623452078a7e4867cf
495174699 <495174699@qq.com>
2021-05-23 16:48:28 +0800

    接口,类的基本知识演练

--- TypeScript/B站教程/js/05-ts中的接口.js
+++ TypeScript/B站教程/js/05-ts中的接口.js
@@ -41,3 +41,55 @@
 // }
 // console.log((md5('12','23')));
 // console.log((md6('12','233')));
+// 类类型接口    实现接口的类必须包含接口中定义的属性和方法
+// interface Animal {
+//     name:string;
+//     eat():void;
+// }
+// class Dog implements Animal{
+//     name:string
+//     constructor (name:string) {
+//         this.name = name
+//     }
+//     eat(value?:string):void {
+//         console.log(this.name+'吃粮食');
+//     }
+// }
+// class Cat implements Animal{
+//     name:string
+//     constructor (name:string) {
+//         this.name = name
+//     }
+//     eat(value?:string):void {
+//         console.log(this.name+'吃粮食');
+//         if(value) {
+//             console.log(this.name+value);
+//         }
+//     }
+// }
+// const dog = new Dog('小狗狗')
+// dog.eat()
+// const cat = new Cat('小猫猫')
+// cat.eat('1212')
+//                                                          接口的扩展,接口的继承
+// class Animal {
+//     name:string
+//     constructor(name:string) {
+//         this.name=name
+//     }
+//     eat():void {
+//     }
+// }
+// interface Person extends Animal{
+// }
+// class Web implements Person {
+//     name:string
+//     constructor(name:string) {
+//         this.name = name
+//     }
+//     eat() {
+//         console.log('吃东西');
+//     }
+// }
+// var web = new Web('哈哈')
+// web.eat()
